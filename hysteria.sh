2bc88c804545d38a5b5c4b8d3e6328ed85ceb4ae
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-07-21 07:48:11 +0000

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hyygV="22.7.16 V 1.0"
+hyygV="22.7.21 V 1.5"
 remoteV=`wget -qO- https://gitlab.com/rwkgyg/hysteria-yg/raw/main/hysteria.sh | sed  -n 2p | cut -d '"' -f 2`
 red='\033[0;31m'
 bblue='\033[0;34m'

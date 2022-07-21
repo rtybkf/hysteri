d548a3d74adcf455ccdc9c4007da4bdb6e35571c
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-07-21 09:36:34 +0000

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -331,6 +331,7 @@ red "未正常安装hysteria!" && exit
 fi
 wget -N https://gitlab.com/rwkgyg/hysteria-yg/raw/main/hysteria.sh
 chmod +x /root/hysteria.sh 
+rm -rf /usr/bin/hy
 ln -sf /root/hysteria.sh /usr/bin/hy
 green "安装脚本升级成功"
 }
